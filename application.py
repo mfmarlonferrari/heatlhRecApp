40f03362dda09cd7a1ea4ec4db7700e7e784d31e
Marlon Ferrari <mfmarlonferrari@gmail.com>
2019-11-20 16:04:13 -0300

    aws cofigs for eb2

--- application.py
+++ application.py
@@ -4,19 +4,19 @@ import os
 import numpy as np
 
 application = flask.Flask(__name__)
-port = int(os.getenv("VCAP_APP_PORT"))
-           
+
 model = pickle.load(open("xgb_card_80auc.pkl","rb"))
    
-@app.route('/predict', methods=['POST'])
+@application.route('/predict', methods=['POST'])
 def predict():
 
    features = flask.request.get_json(force=True)['features']
-   prediction = model.predict([features])
-   probab = model.predict_proba([features])
+   data =  np.array([features])
+   prediction = model.predict(data)
+   probab = model.predict_proba(data)
    response = {'prediction': prediction.tolist(), 'probability':probab.tolist()}
 
    return flask.jsonify(response)
 
 if __name__ == "__main__":
-   application.run(host="0.0.0.0", port=port)
+   application.run()
